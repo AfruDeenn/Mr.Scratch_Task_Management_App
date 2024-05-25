80babb6dc886186f960d9751dc263abb6745f4a8
Afrudeen <143599094+AfruDeenn@users.noreply.github.com>
2024-05-25 14:18:57 +0530

    userRoleName field is added in the UserRole.java file

--- src/main/java/com/TaskScheduler/entity/UserRole.java
+++ src/main/java/com/TaskScheduler/entity/UserRole.java
@@ -1,5 +1,6 @@
 package com.TaskScheduler.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -26,13 +27,16 @@ public class UserRole {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userRoleId;
 
+	@Column(name = "role")
+	private String userRoleName;
+
 	@ManyToOne(fetch = FetchType.LAZY) // User is the owning side
 	@JoinColumn(name = "user_id", nullable = false)
 	private Users user;
 
 	@Override
 	public String toString() {
-		return "UserRole [userRoleId=" + userRoleId + ", user=" + user + "]";
+		return "UserRole [userRoleId=" + userRoleId + ", userRoleName=" + userRoleName + ", user=" + user + "]";
 	}
 
 }
